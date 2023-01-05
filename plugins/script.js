eb826d8f102c8b1c5d417f9e52118246cb434c43
iykeezekwesili <iykeezekwesili@gmail.com>
2023-01-05 22:26:10 +0100

    Add files via upload

--- plugins/script.js
+++ plugins/script.js
@@ -1,50 +1,57 @@
 var monthNames = ["January","February","March","April","May","June","July","August","September","October","November","December"]
-		var dayNames = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-		var tbody = $("tbody")
+var dayNames = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+var tbody = $("tbody")
 		
-			setInterval(function(){
-				var date = new Date()
-				var year = date.getFullYear()
-				var month = date.getMonth()
-				var day = date.getDay()
-				var tempDate = new Date(year, month)
-				var monthStartDay = tempDate.getDay()
-				var totalDays = new Date(year, month+1,0)
-				totalDays = totalDays.getDate()
+setInterval(function(){
+	var date = new Date()
+	var year = date.getFullYear()
+	var month = date.getMonth()
+	var day = date.getDay()
+	var tempDate = new Date(year, month)
+	var monthStartDay = tempDate.getDay()
+	var totalDays = new Date(year, month+1,0)
+	totalDays = totalDays.getDate()
 			
-				tbody.children().remove()
+	tbody.children().remove()
 			
-				for (i=1; i<=6; i++){
-					var tr = $("<tr></tr>")
+	for (i=1; i<=6; i++){
+		var tr = $("<tr></tr>")
 				
-					for (k=1; k<=7; k++){
-						var td = $("<td></td>").text("")
-						tr.append(td)
-					}
+		for (k=1; k<=7; k++){
+			var td = $("<td></td>").text("")
+			tr.append(td)
+		}
 				
-					tbody.append(tr)
-				}
+		tbody.append(tr)
+	}
 			
-				var count = 1
+	var count = 1
 				
-				for (i = monthStartDay; i <= totalDays + monthStartDay - 1; i++){
-					$("tbody td").eq(i).text(count++)
-				}
+	for (i = monthStartDay; i <= totalDays + monthStartDay - 1; i++){
+		$("tbody td").eq(i).text(count++)
+	}
 				
-				$("tbody td")
-					.eq(date.getDate() + monthStartDay-1)
-					.addClass("today")
+	$("tbody td")
+		.eq(date.getDate() + monthStartDay-1)
+		.addClass("today")
 				
-				for(i=5; i>=4; i--){
-					var td = tbody.find("tr").eq(i).children()
+	for(i=5; i>=4; i--){
+		var td = tbody.find("tr").eq(i).children()
 				
-					if (td.eq(0).text() == ""){
-						td.parent().remove()
-					}
+		if (td.eq(0).text() == ""){
+			td.parent().remove()
+		}
 				
-				}
+	}
 				
 					
-				var tableCaption = document.querySelector("caption")
-				tableCaption.textContent = dayNames[day] + ", " + date.getDate() + " " + monthNames[month] + " " + year
-			}, 500)
+	var tableCaption = document.querySelector("caption")
+	tableCaption.textContent = dayNames[day] + ", " + date.getDate() + " " + monthNames[month] + " " + year
+}, 500)
+
+$("#theme").change(function(e){
+	var theme
+	theme = $(this).val()
+			
+	$("table").attr("class", theme)
+})
